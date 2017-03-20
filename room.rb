5c17eedd36173e5ab494e582070fd2f16716384f
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-03-20 09:45:34 +0000

    m

--- room.rb
+++ room.rb
@@ -5,9 +5,41 @@ class Room
   def initialize(name, price, capacity)
     @name = name
     @price = price
-    @capacity - capacity
+    @capacity = capacity
+    @guests = []
+    @songs = []
   end
 
+  def how_many_guests
+    return @guests.length
+  end
 
+  def add_guest_to_room(guest)
+    @guests << guest
+    return @guests.length
+  end
+
+  def remove_guest_from_room(guest)
+    @guests -= [guest]
+    return @guests.length
+  end
+
+  def how_many_songs
+    return @songs.length
+  end
+
+  def add_song_to_room(song)
+    @songs << song
+    return @songs.length
+  end
+
+  def remove_song_from_room(song)
+    @songs -= [song]
+    return @songs.length
+  end
+
+  def empty
+    @guests = []
+  end
 
 end
